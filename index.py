affa8103d60389ee93cb7d756de2e3443afe06f1
saran-CentOS <saran.s6691@gmail.com>
2021-10-12 19:51:12 -0500

    updates on index

--- index.py
+++ index.py
@@ -11,16 +11,21 @@ class index:
         self.tokens = tokens
     def printValues(self):
         print("index {} {}".format(self.docId," ".join(map(str,self.tokens))))
+def checkInt(string):
+    value = int(string)
+    if not isinstance(value,int):
+        raise argparse.ArgumentTypeError('index error doc-id should be int')
+    return value
 
 def main():
     parser = argparse.ArgumentParser(description='Add index for search Engine.')
 
-    parser.add_argument('DocumentID',type=int,help="Document Id of index for the tokens")
+    parser.add_argument('docId',type=int,help="Document Id of index for the tokens",)
     parser.add_argument('Tokens',metavar='T',type=str, nargs='+',help="<Required> Set flag")
     args = parser.parse_args()
 
     try: 
-        documentId = args.DocumentID
+        documentId = args.docId
     except Exception as e:
         print(e)
 
